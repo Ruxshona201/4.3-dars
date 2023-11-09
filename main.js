6b7459e5de081ff5a8d93cd8b235f840d3b3ab35
unknown <ruxshonaabdupattoyeva2010@gmail.com>
2023-11-09 19:14:14 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,45 @@
+// 1.
+let d = + "10";
+let n = 5;
+
+
+let k = d + n;
+console.log(k);
+
+// 2.
+let c = NaN;
+console.log(c); // NaN
+
+// 3.
+typeof 2;
+console.log(typeof 2); // number
+
+// 4.
+let x = false;
+console.log(Boolean(x));
+
+// 5.
+// 1-usul
+let a = 15;
+a = a + 1;
+console.log(a); // 16
+
+// 2-usul
+let b = 16;
+b++;
+console.log(b); // 17
+
+// 6.
+// 1-usul
+let i = 19;
+i = i - 1;
+console.log(i); // 18
+
+// 2-usul
+let l = 20;
+l--;
+console.log(l); // 19
+
+// 7.
+let m = "";
+console.log(m); // null
